87e6547e2023b9a058aff079e63b3b7cc5ed8122
CallumCheng <callumcheng@outlook.com>
2022-08-17 16:09:22 +0100

    refactored .env

--- dbConfig/init.js
+++ dbConfig/init.js
@@ -1,12 +1,10 @@
 const { Pool } = require("pg");
 
-
-
 const pool = new Pool({
     user: 'dfcohocmhpzmmf',
     host: 'ec2-34-252-216-149.eu-west-1.compute.amazonaws.com',
     database: 'dccrt91mtusg2s',
-    password: 'password',
+    password: '01dcaa0f2c797be037b8974ea159b624f06cae909f3da3f276a855d1338b6f93',
     port: 5432,
   })
 
